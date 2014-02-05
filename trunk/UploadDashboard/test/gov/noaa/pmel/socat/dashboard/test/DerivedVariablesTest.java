89b38551377363723040001aa9fa39f8fb962f2d
roland.schweitzer@gmail.com <roland.schweitzer@gmail.com@5741d604-0cbc-a743-4ee9-ff811dc531c8>
2014-02-05 16:03:34 +0000

    Added a test for what it's worth.

--- trunk/UploadDashboard/test/gov/noaa/pmel/socat/dashboard/test/DerivedVariablesTest.java
+++ trunk/UploadDashboard/test/gov/noaa/pmel/socat/dashboard/test/DerivedVariablesTest.java
@@ -1,9 +1,28 @@
 package gov.noaa.pmel.socat.dashboard.test;
 
+import static org.junit.Assert.assertFalse;
 import gov.noaa.pmel.socat.dashboard.ferret.SocatTool;
+
+import org.junit.Test;
 
 public class DerivedVariablesTest {
 
+    @Test
+    public void derivedVariablesTest() {
+        CruiseDsgNcFileTest testfile = new CruiseDsgNcFileTest();
+        try {
+            testfile.testCreate();
+            if ( testfile.getFilename() != null ) {
+                SocatTool tool = new SocatTool();
+                tool.init(testfile.getFilename());
+                tool.run();
+                assertFalse(tool.hasError());
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
     /**
      * @param args
      */
