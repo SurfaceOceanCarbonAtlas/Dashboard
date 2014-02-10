0cdac8efaab0b82ffe6d1efb8dec9bdbe805b2bc
squaregoldfish <squaregoldfish@gmail.com>
2014-02-10 14:42:30 +0000

    Removed deprecated DateMidnight objects.

--- SanityChecker/src/uk/ac/uea/socat/sanitychecker/metadata/InitialSubmissionMetadataItem.java
+++ SanityChecker/src/uk/ac/uea/socat/sanitychecker/metadata/InitialSubmissionMetadataItem.java
@@ -4,7 +4,7 @@ import java.text.ParseException;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
-import org.joda.time.DateMidnight;
+import org.joda.time.DateTime;
 
 import uk.ac.uea.socat.sanitychecker.config.MetadataConfigItem;
 import uk.ac.uea.socat.sanitychecker.data.SocatDataRecord;
@@ -16,7 +16,6 @@ import uk.ac.uea.socat.sanitychecker.data.datetime.DateTimeHandler;
  * Implementation of the {@code MetadataItem} class
  * to handle the Initial Submission Date entry
  */
-@SuppressWarnings("deprecation")
 public class InitialSubmissionMetadataItem extends MetadataItem {
 
 	/**
@@ -37,7 +36,7 @@ public class InitialSubmissionMetadataItem extends MetadataItem {
 		// Otherwise we set today's date.
 		try {
 			if (null == getValue(dateTimeHandler)) {
-				setValue(new DateMidnight());
+				setValue(new DateTime().withTimeAtStartOfDay());
 			}
 		} catch (DateTimeException e) {
 			throw new MetadataException(getName(), "A value already exists for the initial submission date, but it couldn't be retrieved");
