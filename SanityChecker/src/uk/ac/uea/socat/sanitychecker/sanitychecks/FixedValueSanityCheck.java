141856310a74cdeadfcfaa4398ae39f9d266576a
karlmattsmith <karlmattsmith@gmail.com>
2014-06-12 04:30:30 +0000

    Add a second parameter to FixedValueSanityCheck to specify whether missing values/blank entries should be skipped or if they should create an error message.  Set the default configuration for now to ignore the missing for the Expocode and CruiseID columsn for Benjamin's work.

--- SanityChecker/src/uk/ac/uea/socat/sanitychecker/sanitychecks/FixedValueSanityCheck.java
+++ SanityChecker/src/uk/ac/uea/socat/sanitychecker/sanitychecks/FixedValueSanityCheck.java
@@ -6,12 +6,13 @@ import uk.ac.uea.socat.sanitychecker.config.ConfigException;
 import uk.ac.uea.socat.sanitychecker.config.SocatColumnConfig;
 import uk.ac.uea.socat.sanitychecker.config.SocatColumnConfigItem;
 import uk.ac.uea.socat.sanitychecker.config.SocatDataBaseException;
+import uk.ac.uea.socat.sanitychecker.data.SocatDataColumn;
 import uk.ac.uea.socat.sanitychecker.data.SocatDataRecord;
 
 public class FixedValueSanityCheck extends SanityCheck {
 
 	private String itsColumnName = null;
-	
+	private boolean ignoreMissing = false;
 	private String itsFixedValue = null;
 	
 	@Override
@@ -19,8 +20,11 @@ public class FixedValueSanityCheck extends SanityCheck {
 		if (parameters.size() < 1) {
 			throw new SanityCheckException("Must supply column name");
 		}
-		
 		itsColumnName = parameters.get(0);
+		if ( (parameters.size() >= 2) &&
+			 "Y".equalsIgnoreCase(parameters.get(1)) ) {
+			ignoreMissing = true;
+		}
 
 		try {
 			SocatColumnConfig columnConfig = SocatColumnConfig.getInstance();
@@ -33,28 +37,30 @@ public class FixedValueSanityCheck extends SanityCheck {
 	}
 
 	@Override
-	public void processRecord(SocatDataRecord record)
-			throws SanityCheckException {
-		
-		
-		
+	public void processRecord(SocatDataRecord record) throws SanityCheckException {
+
 		String value = record.getColumn(itsColumnName).getValue();
-		
+
 		// We don't check null values
-		if (null != value) {
-			if (null == itsFixedValue) {
-				itsFixedValue = value;
-			} else {
-				if (value.compareTo(itsFixedValue) != 0) {
-					try {
-						String message = "Value for column has changed - it must be the same in all records";
-						record.getColumn(itsColumnName).setFlag(SocatColumnConfigItem.BAD_FLAG, itsMessages, record.getLineNumber(), message);
-					}
-					catch (SocatDataBaseException e) {
-						throw new SanityCheckException ("Error while setting flag on record", e);
-					}
-				}
+		if ( null == value )
+			return;
+		if ( ignoreMissing && ( value.equals(SocatDataColumn.MISSING_VALUE) || value.trim().isEmpty() ) )
+			return;
+
+		if ( null == itsFixedValue ) {
+			itsFixedValue = value;
+			return;
+		}
+
+		if ( value.compareTo(itsFixedValue) != 0 ) {
+			try {
+				String message = "Value for column has changed - it must be the same in all records";
+				record.getColumn(itsColumnName).setFlag(SocatColumnConfigItem.BAD_FLAG, itsMessages, record.getLineNumber(), message);
+			}
+			catch (SocatDataBaseException e) {
+				throw new SanityCheckException ("Error while setting flag on record", e);
 			}
 		}
 	}
+
 }
