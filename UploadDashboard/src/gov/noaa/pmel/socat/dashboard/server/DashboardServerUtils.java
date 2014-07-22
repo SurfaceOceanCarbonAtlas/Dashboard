22bcb2e0b238c635d458ab6f48e6a43763f0c430
karlmattsmith <karlmattsmith@gmail.com>
2014-07-22 23:53:17 +0000

    Remove "cruise" from some error messages.

--- UploadDashboard/src/gov/noaa/pmel/socat/dashboard/server/DashboardServerUtils.java
+++ UploadDashboard/src/gov/noaa/pmel/socat/dashboard/server/DashboardServerUtils.java
@@ -31,19 +31,19 @@ public class DashboardServerUtils {
 	 */
 	public static String checkExpocode(String expocode) throws IllegalArgumentException {
 		if ( expocode == null )
-			throw new IllegalArgumentException("Cruise expocode not given");
+			throw new IllegalArgumentException("Expocode not given");
 		// Do some automatic clean-up
 		String upperExpo = expocode.trim().toUpperCase();
 		// Make sure it is the proper length
 		if ( (upperExpo.length() < DashboardUtils.MIN_EXPOCODE_LENGTH) || 
 			 (upperExpo.length() > DashboardUtils.MAX_EXPOCODE_LENGTH) )
 			throw new IllegalArgumentException(
-					"Invalid cruise Expocode length");
+					"Invalid Expocode length");
 		// Make sure there are no invalid characters
 		Matcher mat = invalidExpocodePattern.matcher(upperExpo);
 		if ( mat.find() )
 			throw new IllegalArgumentException(
-					"Invalid characters in the cruise Expocode");
+					"Invalid characters in the Expocode");
 		return upperExpo;
 	}
 
