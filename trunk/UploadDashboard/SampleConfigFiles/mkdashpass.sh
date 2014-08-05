51a1cd17fa376798d04e9b83355826ae51675b97
karlmattsmith@gmail.com <karlmattsmith@gmail.com@5741d604-0cbc-a743-4ee9-ff811dc531c8>
2014-08-05 20:32:08 +0000

    Updating sample configuration files

--- trunk/UploadDashboard/SampleConfigFiles/mkdashpass.sh
+++ trunk/UploadDashboard/SampleConfigFiles/mkdashpass.sh
@@ -1,24 +1,17 @@
 #! /bin/sh
-if [ -n "${DASHBOARD_WEBINF}" ]; then
-    if [ -z "${CATALINA_HOME}" ]; then
-        export CATALINA_HOME="${HOME}"
-    fi
+if [ -n "${CATALINA_BASE}" ]; then
+    DASHBOARD_WEBINF="${CATALINA_BASE}/webapps/SocatUploadDashboard/WEB-INF"
 elif [ -n "${CATALINA_HOME}" ]; then
-    export DASHBOARD_WEBINF="${CATALINA_HOME}/webapps/SocatUploadDashboard/WEB-INF"
+    CATALINA_BASE="${CATALINA_HOME}"
+    DASHBOARD_WEBINF="${CATALINA_HOME}/webapps/SocatUploadDashboard/WEB-INF"
 else
+    CATALINA_BASE="${HOME}"
+    DASHBOARD_WEBINF="${HOME}/workspace/SocatUploadDashboard/war/WEB-INF"
+fi
+
+if [ ! -d "${CATALINA_BASE}/content" ]; then
     echo ''
-    echo 'Define DASHBOARD_WEBINF to be the full path of the WEB_INF '
-    echo 'directory under webapps/SocatUploadDashboard.  The '
-    echo 'content/SocatUploadDashboard/SocatUploadDashboard.properties '
-    echo 'file under $CATALINA_HOME, if defined, or $HOME will used for '
-    echo 'encryption keys. '
-    echo ''
-    echo 'Alternatively define CATALINA_HOME to be the full path of '
-    echo 'the tomcat home directory containing webapps, in which case '
-    echo '$CATALINA_HOME/webapps/SocatUploadDashboard/WEB-INF will be '
-    echo 'used for DASHBOARD_WEBINF and, as before, the dashboard '
-    echo 'properties file under this directory will be used for the '
-    echo 'encryption keys. '
+    echo '${CATALINA_BASE}/content not found'
     echo ''
     exit 1
 fi
@@ -36,4 +29,4 @@ if [ ! -d "${DASHBOARD_WEBINF}/lib" ]; then
 fi
 
 export CLASSPATH="${DASHBOARD_WEBINF}/classes:${DASHBOARD_WEBINF}/lib/*"
-java -Dcatalina.base="${CATALINA_HOME}" gov.noaa.pmel.socat.dashboard.server.DashboardDataStore "$@"
+java -Dcatalina.base="${CATALINA_BASE}" gov.noaa.pmel.socat.dashboard.server.DashboardDataStore "$@"
