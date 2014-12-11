b35c8cd7ffe3479928ad3cd935dadc407f589fae
karlmattsmith <karlmattsmith@gmail.com>
2014-12-11 19:35:30 +0000

    When only two consecutive row numbers, show as comma-separated instead of hyphen-separated.  Tweak the columns header in the WOCE flags file.

--- UploadDashboard/test/gov/noaa/pmel/socat/dashboard/test/RowNumSetTest.java
+++ UploadDashboard/test/gov/noaa/pmel/socat/dashboard/test/RowNumSetTest.java
@@ -25,22 +25,25 @@ public class RowNumSetTest {
 		rowSet.add(5);
 		assertEquals("5", rowSet.toString());
 		rowSet.add(6);
-		assertEquals("5-6", rowSet.toString());
+		assertEquals("5,6", rowSet.toString());
 		rowSet.add(7);
+		assertEquals("5-7", rowSet.toString());
 		rowSet.add(8);
 		rowSet.add(9);
 		assertEquals("5-9", rowSet.toString());
 		rowSet.add(12);
-		assertEquals("5-9, 12", rowSet.toString());
+		assertEquals("5-9,12", rowSet.toString());
 		for (int k = 55; k < 67; k++)
 			rowSet.add(k);
-		assertEquals("5-9, 12, 55-66", rowSet.toString());
+		assertEquals("5-9,12,55-66", rowSet.toString());
 		rowSet.add(2);
-		assertEquals("2, 5-9, 12, 55-66", rowSet.toString());
+		assertEquals("2,5-9,12,55-66", rowSet.toString());
 		rowSet.add(98);
-		assertEquals("2, 5-9, 12, 55-66, 98", rowSet.toString());
+		assertEquals("2,5-9,12,55-66,98", rowSet.toString());
 		rowSet.add(60);
-		assertEquals("2, 5-9, 12, 55-66, 98", rowSet.toString());
+		assertEquals("2,5-9,12,55-66,98", rowSet.toString());
+		rowSet.add(3);
+		assertEquals("2,3,5-9,12,55-66,98", rowSet.toString());
 	}
 
 }
