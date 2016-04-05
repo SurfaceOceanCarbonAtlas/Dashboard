de7f5ae6e6a66770428fb6a054e4aeb54dd48204
Steve Jones <steve@squaregoldfish.co.uk>
2016-04-05 12:03:31 +0100

    SocatDataRecord inherits from QC_Routines DataRecord

--- SanityChecker/src/uk/ac/uea/socat/sanitychecker/data/calculate/ExpoCode.java
+++ SanityChecker/src/uk/ac/uea/socat/sanitychecker/data/calculate/ExpoCode.java
@@ -1,28 +1,19 @@
 package uk.ac.uea.socat.sanitychecker.data.calculate;
 
+import uk.ac.exeter.QCRoutines.data.NoSuchColumnException;
 import uk.ac.uea.socat.omemetadata.OmeMetadata;
 import uk.ac.uea.socat.omemetadata.OmeMetadataException;
 import uk.ac.uea.socat.sanitychecker.data.SocatDataException;
 import uk.ac.uea.socat.sanitychecker.data.SocatDataRecord;
 import uk.ac.uea.socat.sanitychecker.data.datetime.DateTimeHandler;
-import uk.ac.uea.socat.sanitychecker.messages.Message;
-import uk.ac.uea.socat.sanitychecker.messages.MessageType;
 
 public class ExpoCode implements DataCalculator {
 
-	private static final String CANT_CALC_EXPOCODE_ID = "CANT_CALC_EXPOCODE";
-	
-	private static MessageType CANT_CALC_EXPOCODE_TYPE = null; 
-	
 	/**
 	 * Calculates the EXPO code for the cruise 
 	 */
 	@Override
 	public String calculateDataValue(OmeMetadata metadata, SocatDataRecord record, int colIndex, String colName, DateTimeHandler dateTimeHandler) throws SocatDataException, OmeMetadataException {
-		
-		if (null == CANT_CALC_EXPOCODE_TYPE) {
-			CANT_CALC_EXPOCODE_TYPE = new MessageType(CANT_CALC_EXPOCODE_ID, "Cannot calculate EXPO Code: Ship Code or Start Date are missing from metadata", "Cannot calculate EXPO Code");
-		}
 		
 		/*
 		 * The metadata processor will have already determined that the required metadata fields are
@@ -38,8 +29,11 @@ public class ExpoCode implements DataCalculator {
 			result = expocode;
 		} else {
 			if (null == shipCode || null == startDate) {
-				Message message = new Message(Message.NO_COLUMN_INDEX, null, CANT_CALC_EXPOCODE_TYPE, Message.ERROR, Message.NO_LINE_NUMBER, null, null);
-				record.addMessage(message);
+				try {
+					record.addMessage(new CantCalculateExpoCodeMessage());
+				} catch(NoSuchColumnException e) {
+					throw new SocatDataException(record.getLineNumber(), colIndex, null, "Error while adding message to record", e); 
+				}
 			} else { 
 				result = shipCode + startDate;
 			}
