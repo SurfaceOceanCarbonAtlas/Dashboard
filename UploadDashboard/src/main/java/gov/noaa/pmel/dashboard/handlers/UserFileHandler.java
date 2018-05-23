00ba786683ae406e7d7ce7787fdacc43bdbf7532
karlmsmith <karl.smith@noaa.gov>
2018-05-23 13:20:26 -0700

    Fix typo

--- UploadDashboard/src/main/java/gov/noaa/pmel/dashboard/handlers/UserFileHandler.java
+++ UploadDashboard/src/main/java/gov/noaa/pmel/dashboard/handlers/UserFileHandler.java
@@ -111,7 +111,7 @@ public class UserFileHandler extends VersionedFileHandler {
                 throw new IllegalArgumentException("Unknown data type \"" +
                         vals[0] + "\" for tag \"" + colName + "\"");
             DataColumnType dctype = dtype.duplicate();
-            if ( !dctype.setSelectedUnit(vals[1]) ) {
+            if ( !dctype.setSelectedUnit(vals[1]) )
                 throw new IllegalArgumentException("Unknown data unit \"" +
                         vals[1] + "\" for data type \"" + vals[0] + "\"");
             dctype.setSelectedMissingValue(vals[2]);
