f5a46df9c029a231f5ba45386c17e1307214eb57
karlmsmith <karl.smith@noaa.gov>
2020-04-28 11:23:50 -0700

    update the archive status and timestamp when manually generating the original file bundles (for archiving at release time for those set to "archive with next release")

--- DashboardApps/src/main/java/gov/noaa/pmel/dashboard/programs/GenerateOrigFileBundles.java
+++ DashboardApps/src/main/java/gov/noaa/pmel/dashboard/programs/GenerateOrigFileBundles.java
@@ -4,12 +4,19 @@
 package gov.noaa.pmel.dashboard.programs;
 
 import gov.noaa.pmel.dashboard.handlers.ArchiveFilesBundler;
+import gov.noaa.pmel.dashboard.handlers.DataFileHandler;
 import gov.noaa.pmel.dashboard.server.DashboardConfigStore;
 import gov.noaa.pmel.dashboard.server.DashboardServerUtils;
+import gov.noaa.pmel.dashboard.shared.DashboardDataset;
+import gov.noaa.pmel.dashboard.shared.DashboardUtils;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.text.spi.DateFormatProvider;
+import java.util.Date;
 import java.util.TreeSet;
 
 /**
@@ -34,10 +41,10 @@ public class GenerateOrigFileBundles {
         if ( args.length != 1 ) {
             System.err.println("Arguments:  IDsFile");
             System.err.println();
-            System.err.println("Generates original data file bundles for the dataset IDs specified ");
-            System.err.println("in IDsFile.  These file bundles are added to the version control ");
-            System.err.println("bundles directory, but are not emailed to anyone.  The default ");
-            System.err.println("dashboard configuration is used for this process. ");
+            System.err.println("Generates original data file bundles for the dataset IDs specified in IDsFile. ");
+            System.err.println("These file bundles are added to the version control bundles directory, but are ");
+            System.err.println("not emailed to anyone.  The default dashboard configuration specified by the ");
+            System.err.println("environment variable UPLOAD_DASHBOARD_SERVER_NAME is used for this process. ");
             System.err.println();
             System.exit(1);
         }
@@ -75,16 +82,26 @@ public class GenerateOrigFileBundles {
         try {
 
             ArchiveFilesBundler filesBundler = configStore.getArchiveFilesBundler();
+            DataFileHandler dataHandler = configStore.getDataFileHandler();
+            String archiveStatus = DashboardUtils.ARCHIVE_STATUS_SENT_TO_START + "OCADS";
+            String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm Z").format(new Date());
             for (String datasetId : idsSet) {
                 String commitMsg = "Automated generation of the original data files bundle for " + datasetId;
                 try {
                     String resultMsg = filesBundler.sendOrigFilesBundle(datasetId, commitMsg,
                             DashboardServerUtils.NOMAIL_USER_REAL_NAME, DashboardServerUtils.NOMAIL_USER_EMAIL);
+                    // Output the (success) message from the archival
                     System.out.println(datasetId + " : " + resultMsg);
+                    // Update the archived timestamp
+                    DashboardDataset cruise = dataHandler.getDatasetFromInfoFile(datasetId);
+                    cruise.setArchiveStatus(archiveStatus);
+                    cruise.getArchiveTimestamps().add(timestamp);
+                    dataHandler.saveDatasetInfoToFile(cruise, commitMsg);
                 } catch ( IllegalArgumentException | IOException ex ) {
                     System.out.println(datasetId + " : " + "failed - " + ex.getMessage());
                     success = false;
                 }
+
             }
 
         } finally {
